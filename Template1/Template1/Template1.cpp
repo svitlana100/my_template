9ccca387ea0deec3a344e74226e8a2191923d82b
Svitlana200 <svitlana.I.yatsyshyn@lpnu.ua>
2021-11-17 10:36:49 +0200

    template__int

--- Template1/Template1/Template1.cpp
+++ Template1/Template1/Template1.cpp
@@ -43,6 +43,28 @@ public:
 
 };
 template <>
+class Repository5<int>
+{
+private:
+	unsigned char m_data;
+public:
+	Repository5() : m_data(0) {}
+	void set(int index, bool value)
+	{
+		unsigned char mask = 1 << index;
+		if (value) m_data != mask;
+		else m_data &= ~mask;
+	}
+	bool get(int index)
+	{
+		unsigned char mask = 1 << index;
+		return (m_data&mask) != 0;
+	}
+
+
+};
+
+template <>
 class Repository5<double>
 {
 private:
